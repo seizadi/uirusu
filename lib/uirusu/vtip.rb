0d8e95ba5c1c745921ac9949a947eb4ef97dc35b
Soheil Eizadi <seizadi@infoblox.com>
2015-12-16 13:09:52 -0800

    Added IP Address Report

---
+++ lib/uirusu/vtip.rb
@@ -0,0 +1,63 @@
+# Copyright (c) 2012-2016 Arxopia LLC.
+# Copyright (c) Soheil Eizadi
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Arxopia LLC nor the names of its contributors
+#     	may be used to endorse or promote products derived from this software
+#     	without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL ARXOPIA LLC BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+module Uirusu
+	#
+	#
+	module VTIp
+		REPORT_IP = Uirusu::VT_API + "/ip-address/report"
+
+		# Submits an IP Address report request to Virustotal.com
+		#
+		# @param api_key Virustotal.com API key
+		# @param resource IP Address to submit
+		#
+		# @return [JSON] Parsed response
+		def self.report_ip(api_key, resource)
+			if api_key == nil
+				raise "Invalid API Key"
+			end
+
+			if resource == nil
+				raise "Invalid resource, must be a valid IP Address"
+			end
+
+			response = RestClient.get REPORT_IP, :apikey => api_key, :ip => resource
+
+			case response.code
+				when 429, 204
+					raise "Virustotal limit reached. Try again later."
+				when 403
+					raise "Invalid privileges, please check your API key."
+				when 200
+					JSON.parse(response)
+				else
+					raise "Unknown Server error."
+			end
+		end
+	end
+end
